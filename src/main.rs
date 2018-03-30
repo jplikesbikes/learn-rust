ebbd03bff14617596e2e1b343e67891510882bba
jp <jplikesbikes@gmail.com>
2018-03-29 22:28:51 -0400

    read the version from cargos build environment

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ extern crate learn_rust;
 use learn_rust::db;
 
 fn main() {
+	let version = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
+
+	println!("Version: {}", version);
 	db::create();
 }
