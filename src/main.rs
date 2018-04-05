2c4ca1cf6f8c321afe6184c9a6b4706fa890b6a0
jp <jplikesbikes@gmail.com>
2018-04-04 23:13:46 -0400

    using a logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate learn_rust;
 
 use learn_rust::db;
 
 fn main() {
+	// initialize the logger implementation
+	env_logger::init();
+
 	let version = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
 
-	println!("Version: {}", version);
+	info!("Version: {}", version);
 	db::create();
 }
