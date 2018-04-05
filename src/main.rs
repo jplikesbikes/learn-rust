4d0e26084e5656eeae76d48f1c5153e514b9058c
jp <jplikesbikes@gmail.com>
2018-04-04 23:31:42 -0400

    adding error handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate error_chain;
 extern crate env_logger;
 extern crate learn_rust;
 
 use learn_rust::db;
+use errors::*;
+
+mod errors {
+	// Create the Error, ErrorKind, ResultExt, and ResultTypes
+	error_chain!{}
+}
 
 fn main() {
 	// initialize the logger implementation
 	env_logger::init();
 
+	if let Err(ref e) = run() {
+		println!("error: {}", e);
+
+		for e in e.iter().skip(1) {
+			println!("caused by: {}", e);
+		}
+
+		// The backtrace is not always generated. Run this with `RUST_BACKTRACE=1`.
+		if let Some(backtrace) = e.backtrace() {
+			println!("backtrace: {:?}", backtrace);
+		}
+
+		::std::process::exit(1);
+	}
+}
+
+fn run() -> Result<()> {
 	let version = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
 
 	info!("Version: {}", version);
 	db::create();
+
+	use std::fs::File;
+
+	// This operation will fail
+	File::open("contacts").chain_err(|| "unable to open contacts file")?;
+
+	Ok(())
 }
