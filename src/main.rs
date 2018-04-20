9bcdde941e4e8fcbfb6cf3bea65c3b80cd6fc1c4
jp <jplikesbikes@gmail.com>
2018-04-19 22:50:44 -0400

    use error_chains quick_main macro

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,7 @@ mod errors {
 	error_chain!{}
 }
 
-fn main() {
-	// initialize the logger implementation
-	env_logger::init();
-
-	if let Err(ref e) = run() {
-		println!("error: {}", e);
-
-		for e in e.iter().skip(1) {
-			println!("caused by: {}", e);
-		}
-
-		// The backtrace is not always generated. Run this with `RUST_BACKTRACE=1`.
-		if let Some(backtrace) = e.backtrace() {
-			println!("backtrace: {:?}", backtrace);
-		}
-
-		::std::process::exit(1);
-	}
-}
+quick_main!(run);
 
 fn run() -> Result<()> {
 	let version = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
